85b0a30ecb2cbb64e47add67af1a875831396b06
Abou-Taleb, Rabie Zamane <rabie.abou-taleb@gim.biologisgroup.com>
2015-08-13 14:35:22 +0200

    Fixes to handle new packages offered bei zarafa sins version 7.13. It will install all packages from the downloaded zarafa archiv.
    kyotocabinet-bin: NEW
    libical-dev: NEW
    libkyotocabinet-dev: NEW
    libvmime-dev: NEW
    pacemaker-zarafa: NEW
    zarafa-multiserver: NEW
    zarafa-webaccess-muc: NEW

--- custom/debian_zarafa_update.sh
+++ custom/debian_zarafa_update.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-SERVICES="postfix"
+/etc/init.d/postfix stop
+
+SERVICES="postfix "
 PACKAGES=" "
 for DEB in `ls *.deb`
 do
   PACKAGE=`dpkg -f $DEB Package`
-  STATUS=`dpkg -s $PACKAGE | grep Status: | cut -d ' ' -f 4`
+  if [ "$(dpkg --list | grep $PACKAGE | wc -l)" = "0" ]
+  then
+    STATUS='NEW'
+  else
+    STATUS=`dpkg -s $PACKAGE | grep Status: | cut -d ' ' -f 4` 
+  fi
+ 
   echo "$PACKAGE: $STATUS"
-  if [ 'installed' = $STATUS ]
+  if [ 'installed' == $STATUS ] || [  'NEW' == $STATUS ]
   then
     PACKAGES="$DEB $PACKAGES"
     if [ -e /etc/init.d/$PACKAGE ]
@@ -26,3 +34,5 @@ done
 dpkg $1 -i $PACKAGES
 
 /etc/init.d/postfix start
+
+/etc/init.d/apache2 restart
